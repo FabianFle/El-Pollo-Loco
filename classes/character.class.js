82af6c4d8d94b6b42b612a1604924f00afd96542
FabianFle <fabianflegler1@yahoo.de>
2022-09-12 20:58:52 +0200

    Objects added, Img added, x - y edit

--- classes/character.class.js
+++ classes/character.class.js
@@ -1,7 +1,14 @@
 class Character extends MovableObject {
-    
 
-    jump(){
+    y = 140;
+    width = 150;
+    height = 290;
+
+    constructor() {
+        super().loadImage('img/2_character_pepe/2_walk/W-21.png');
+    }
+
+    jump() {
 
     }
 }
