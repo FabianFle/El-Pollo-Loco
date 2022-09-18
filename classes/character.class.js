3d5e4c565b21825eb31231d66ce0c52a4033b5a8
FabianFle <fabianflegler1@yahoo.de>
2022-09-18 18:43:04 +0200

    update - Sound addet, animat addet

--- classes/character.class.js
+++ classes/character.class.js
@@ -3,9 +3,55 @@ class Character extends MovableObject {
     y = 140;
     width = 150;
     height = 290;
+    speed = 10;
+
+    images_Walking = [
+        'img/2_character_pepe/2_walk/W-21.png',
+        'img/2_character_pepe/2_walk/W-22.png',
+        'img/2_character_pepe/2_walk/W-23.png',
+        'img/2_character_pepe/2_walk/W-24.png',
+        'img/2_character_pepe/2_walk/W-25.png',
+        'img/2_character_pepe/2_walk/W-26.png'
+    ];
+
+    world;
+
+    walking_sound = new Audio('audio/running.mp3')
 
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
+        this.loadImages(this.images_Walking);
+
+        this.animate();
+    }
+
+    animate() {
+
+        setInterval(() => {
+            this.walking_sound.pause();
+            if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
+                this.x += this.speed;
+                this.otherDirection = false;
+                this.walking_sound.play();
+            }
+
+            if (this.world.keyboard.LEFT && this.x > 0) {
+                this.x -= this.speed;
+                this.otherDirection = true;
+                this.walking_sound.play();
+            }
+            this.world.camera_x = -this.x + 100;
+        }, 1000 / 60);
+
+        setInterval(() => {
+
+            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                    let i = this.currentImage % this.images_Walking.length;
+                let path = this.images_Walking[i];
+                this.img = this.imageCache[path];
+                this.currentImage++;
+            }
+        }, 50);
     }
 
     jump() {
--- classes/movable-object.class.js
+++ classes/movable-object.class.js
@@ -4,18 +4,33 @@ class MovableObject {
     img;
     height = 220;
     width = 150;
+    currentImage = 0;
+    imageCache = {}; 
+    speed = 0.15;
+    otherDirection = false;
 
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
     }
 
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        });
+    }
+
 
     moveRight() {
-
+        console.log('Moving right');
     }
 
-    moveLeft() {
-
-    }
+    moveLeft() 
+        {
+            setInterval(() => {
+                this.x -= this.speed;
+            }, 1000 / 60);
+        }
 }
